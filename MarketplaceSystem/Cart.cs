673b7e053f1b575f3866ceba60e642dbfbfee63d
mostafaesmaeili0990 <mostafaesmaeili0990@gmail.com>
2026-07-22 12:49:53 +0330

    add cart class

---
+++ MarketplaceSystem/Cart.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketplaceSystem
+{
+    internal class Cart
+    {
+        private List<Product> _products = new List<Product>();
+
+        List<Product> Products
+        {
+            get
+            {
+                return _products;
+            }
+            set
+            {
+                _products = value;
+            }
+        }
+
+        public void AddProduct(Product product){ 
+            Products.Add(product);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine();
+            Console.ResetColor();
+        }
+    }
+}
